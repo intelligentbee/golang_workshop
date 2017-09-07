e30bf2488a68ad24df7fdac93559b91f37526292
Laur <laurentiu.craciun@intelligentbee.com>
2017-09-07 11:55:17 +0300

    updated middleware sample

--- web_services/middlewares.go
+++ web_services/middlewares.go
@@ -9,36 +9,37 @@ import (
 	"github.com/urfave/negroni"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "index controller\n")
-	fmt.Printf("index controller\n")
+// handler function for index endpoint
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the content of the index controller\n")
+	log.Println("executing index controller")
 }
 
-//func hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-func (c *Controller) hello(w http.ResponseWriter, r *http.Request) {
+// handler function for /login endpoint
+func loginHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprintf(w, "This is the content of the login controller\n")
+	log.Println("executing login controller")
+}
+
+// handler function for /hello/:name endpoint
+func (c *Controller) helloHandler(w http.ResponseWriter, r *http.Request) {
 	params := c.getUrlParams(r)
 	fmt.Fprintf(w, "Hello, %s!\n", params.ByName("name"))
+	log.Println("executing hello controller")
 }
 
 func (c *Controller) getUrlParams(r *http.Request) httprouter.Params {
 	_, params, _ := c.Router.Lookup("GET", r.URL.Path)
+
 	return params
-
-}
-
-func Panic(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	log.Panic("E panica man!\n")
-}
-
-func loginHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Println("LoginIn")
-	fmt.Printf(">>>>>>>>ps: %+v\n", ps)
 }
 
 func auth(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	// do some stuff before
 	fmt.Printf("auth middleware -> before controller\n")
+
 	next(rw, r)
+
 	fmt.Printf("auth middleware -> after controller\n")
 	// do some stuff after
 }
@@ -49,13 +50,12 @@ type Controller struct {
 
 func main() {
 	router := httprouter.New()
-	router.GET("/panic", Panic)
 	router.POST("/login", loginHandler)
 
 	// add middleware for a specific route
 	nIndex := negroni.New()
 	nIndex.Use(negroni.HandlerFunc(auth))
-	nIndex.UseHandlerFunc(index)
+	nIndex.UseHandlerFunc(indexHandler)
 	router.Handler("GET", "/", nIndex)
 
 	controller := Controller{
@@ -63,7 +63,7 @@ func main() {
 	}
 	nHello := negroni.New()
 	nHello.Use(negroni.HandlerFunc(auth))
-	nHello.UseHandlerFunc(controller.hello)
+	nHello.UseHandlerFunc(controller.helloHandler)
 	router.Handler("GET", "/hello/:name", nHello)
 
 	n := negroni.Classic() // Includes some default middlewares
